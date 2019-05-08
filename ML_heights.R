5417f47e3ffd8659e6fda99fe6006ce2d7f58b8e
Ryan McCoy <rmccoy@CGI.local>
2019-05-08 14:56:32 -0500

    New Iterations of ML Heights Code - Learned to run them independantly and as a whole. Only works when total code is run, not just 1 line at a time.

--- ML_heights.R
+++ ML_heights.R
@@ -1,22 +1,45 @@
 library(caret)
 library(dslabs)
-
+library(purrr)
 
 data(heights)
-
-heights
 
 y <- heights$sex
 x <- heights$height
 
 set.seed(2)
 test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
-train_set <- heights[-test_index, ]
-test_set <- heights[test_index, ]
 
-y_hat <- sample(c("Male","Female"), length(test_index), replace = TRUE) %>%
-  factor(level = levels(test_set$sex))
+test_set <- heights[test_index, ]
+train_set <- heights[-test_index, ]
+
+y_hat <- sample(c("Male", "Female"),length(test_index), replace = TRUE)
+
+y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE) %>% 
+  factor(levels = levels(test_set$sex))
 
 mean(y_hat == test_set$sex)
 
+heights %>% group_by(sex) %>% summarize(mean(height), sd(height))
 
+y_hat <- ifelse(x > 62, "Male", "Female") %>% factor(levels = levels(test_set$sex))
+mean(y == y_hat)
+
+cutoff <- seq(61, 70)
+accuracy <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  mean(y_hat == train_set$sex)
+})
+plot(accuracy, type = "b", col = "blue")
+
+max(accuracy)
+
+best_cutoff <- cutoff[which.max(accuracy)]
+best_cutoff
+
+
+y_hat <- ifelse(test_set$height > best_cutoff, "Male", "Female") %>% 
+  factor(levels = levels(test_set$sex))
+y_hat <- factor(y_hat)
+mean(y_hat == test_set$sex)
